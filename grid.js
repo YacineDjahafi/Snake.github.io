182e108942ecfc04a4457e29f642917dcbebc86d
Neoyacine <neoyacine13@gmail.com>
2023-04-04 16:48:49 +0200

    update 2.3

--- grid.js
+++ grid.js
@@ -6,14 +6,6 @@ export function randomGridPosition(){
         y: Math.floor(Math.random() * GRID_SIZE) + 1
     }
 }
-const GRID_SIZE = 21
-
-export function randomGridPosition(){
-    return{
-        x: Math.floor(Math.random() * GRID_SIZE) + 1,
-        y: Math.floor(Math.random() * GRID_SIZE) + 1
-    }
-}
 
 export function outsideGrid(position) {
     return (
